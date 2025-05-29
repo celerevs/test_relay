8db6d27e69570cb7e156af40027031b2b5d64ecb
celerevs <166853148+celerevs@users.noreply.github.com>
2025-05-29 20:07:51 +0530

    Update relay_test.js

--- relay_test.js
+++ relay_test.js
@@ -4,7 +4,7 @@ const mqtt = require('mqtt');
 const app = express();
 app.use(express.json());
 
-const mqttClient = mqtt.connect('mqtt://YOUR_BROKER_IP:1883');
+const mqttClient = mqtt.connect('mqtt://broker.hivemq.com:1883');
 
 mqttClient.on('connect', () => {
   console.log('Connected to MQTT broker');
@@ -12,7 +12,7 @@ mqttClient.on('connect', () => {
 
 app.post('/webhook', (req, res) => {
   const payload = JSON.stringify(req.body);
-  mqttClient.publish('esp32/trigger', payload);
+  mqttClient.publish('esp32/trigger/upi', payload);
   console.log('Webhook forwarded to MQTT:', payload);
   res.sendStatus(200);
 });
