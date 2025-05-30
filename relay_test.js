be2f0e6ca2e5b713a199f73404cc74aacf820690
celerevs <166853148+celerevs@users.noreply.github.com>
2025-05-30 21:42:46 +0530

    Update relay_test.js

--- relay_test.js
+++ relay_test.js
@@ -16,8 +16,8 @@ mqttClient.on('connect', () => {
 
 app.post('/webhook', (req, res) => {
   const obj = req.body;
-  if(obj.event == 'qr_code.created'){
-    mqttClient.publish('charger/evamp/mini/minitest04', obj.payload?.qr_code?.entity?.id);
+  if(obj.event == 'qr_code.credited'){
+    mqttClient.publish('charger/evamp/mini/minitest04', obj.payload?.payment?.entity?.amount);
   }
   console.log('Webhook forwarded to MQTT:', obj.payload);
   res.sendStatus(200);
