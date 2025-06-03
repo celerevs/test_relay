879f4a0c8e284ac93a3879e0d7f0c4050364bf9a
celerevs <166853148+celerevs@users.noreply.github.com>
2025-06-03 17:28:00 +0530

    Update relay_test.js

--- relay_test.js
+++ relay_test.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const mqtt = require('mqtt');
+// const Razorpay = require('razorpay');
+
+// const razorpay = new Razorpay({
+//   key_id: 'rzp_test_YOUR_KEY_ID',      // Replace with your Razorpay Key ID
+//   key_secret: 'YOUR_KEY_SECRET'        // Replace with your Razorpay Key Secret
+// });
 
 const app = express();
 app.use(express.json());
+
+
 
 const port = process.env.PORT || 4000
 
@@ -12,7 +20,15 @@ const mqttClient = mqtt.connect('mqtt://broker.hivemq.com:1883', {
 
 mqttClient.on('connect', () => {
   console.log('Connected to MQTT broker');
+  mqttClient.subscribe('refund/evamp/mini/', (err) => {
+    if (err) {
+      console.error('Subscribe error:', err);
+    } else {
+      console.log('Subscribed to topic');
+    }
+  });
 });
+
 
 app.post('/webhook', (req, res) => {
   const obj = req.body;
@@ -22,14 +38,35 @@ app.post('/webhook', (req, res) => {
 
   var topic = 'charger/evamp/mini/';
   topic += String(obj.payload?.qr_code?.entity?.id);
-  console.log(topic);
   if(obj.event == 'qr_code.credited'){
     mqttClient.publish(topic, data);
   }
-  console.log('Webhook forwarded to MQTT:', obj.payload);
+  console.log('Webhook forwarded to MQTT:', obj.payload?.qr_code?.entity?.id);
   res.sendStatus(200);
 });
 
 app.listen(port, () => {
-  console.log('Relay server running on port 3000');
+  console.log('Relay server running on port:', port);
+});
+
+// async function refundPayment(paymentId, amountInRupees = null) {
+//   try {
+//     const refundOptions = { payment_id: paymentId };
+
+//     // If partial refund, convert to paise (1 INR = 100 paise)
+//     if (amountInRupees !== null) {
+//       refundOptions.amount = amountInRupees;
+//     }
+
+//     const refund = await razorpay.refunds.create(refundOptions);
+//     console.log('Refund Successful:', refund);
+//   } catch (error) {
+//     console.error('Refund Failed:', error.error?.description || error.message);
+//   }
+// }
+
+mqttClient.on('message', (topic, message) => {
+  const [amount, payment_Id] = message.toString().split(',');
+  console.log('Amount:', amount);         // ➤ "199"
+  console.log('Payment ID:', payment_Id);
 });
